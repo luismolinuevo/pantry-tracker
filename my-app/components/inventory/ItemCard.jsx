989993f8105a693f9eedae67a66fcd08f1acb816
Luis Molinuevo <luismolinuevo0@gmail.com>
2024-08-03 20:05:04 -0400

    clean up card component

--- my-app/components/inventory/ItemCard.jsx
+++ my-app/components/inventory/ItemCard.jsx
@@ -2,11 +2,6 @@
 
 import {
   Typography,
-  Menu,
-  MenuHandler,
-  MenuList,
-  MenuItem,
-  Button,
   Card,
   CardHeader,
   CardBody,
@@ -16,37 +11,6 @@ import React from "react";
 import ItemCardMenu from "./ItemCardMenu";
 
 export default function ItemCard({ item, onChange, setOnChange }) {
-  // return (
-  //   <div className="w-[300px] md:w-[700px] border-[1px] border-black mx-10 my-10 px-5 py-5 rounded-xl">
-  //     <div className="flex justify-between items-center">
-  //       <div>
-  //         <Typography className="text-center text-[20px]">Item Name</Typography>
-  //         <Typography className="text-center text-[25px]">
-  //           {item?.name}
-  //         </Typography>
-  //       </div>
-  //       <div>
-  //         <Typography className="text-center text-[20px]">Count</Typography>
-  //         <Typography className="text-center text-[25px]">
-  //           {item?.count}
-  //         </Typography>
-  //       </div>
-  //       <div>
-  //         <Typography className="text-center text-[20px]">
-  //           Price Per Item
-  //         </Typography>
-  //         <Typography className="text-center text-[25px]">
-  //           {item?.price}
-  //         </Typography>
-  //       </div>
-  //       <ItemCardMenu
-  //         item={item}
-  //         setOnChange={setOnChange}
-  //         onChange={onChange}
-  //       />
-  //     </div>
-  //   </div>
-  // );
   return (
     <Card className="mt-6 w-80">
       <CardHeader color="blue-gray" className="relative h-48">
