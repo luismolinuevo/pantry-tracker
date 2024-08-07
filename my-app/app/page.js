be5d5cea265e9dfb0618990d1842ab3686b07a27
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2024-08-07 00:40:34 -0400

    created demo button

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,6 +1,33 @@
+import { Typography, Button } from "../material_tailwind";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  const demoLogin = async () => {
+    
+  };
+
   return (
-    <main>
+    <main className="flex items-center h-full px-10 py-10">
+      <div className="flex justify-between">
+        <div>
+          <Typography variant="h1" className="text-[50px]">
+            Pantry Tracker
+          </Typography>
+          <Typography className="text-[30px]">
+            A Pantry Tracker is a convenient app that helps you manage your
+            pantry inventory, track expiration dates, and plan meals
+            efficiently. Stay organized and reduce food waste by knowing exactly
+            what you have on hand. Perfect for keeping your kitchen stocked and
+            your shopping list updated!
+          </Typography>
+          <Button onClick="/login">Login</Button>
+          <Button onClick={demoLogin}>Demo</Button>
+        </div>
+        <img src="/Landingpage.jpeg" className="w-[45%] h-[400px]" />
+      </div>
     </main>
   );
 }
---
+++ my-app/components/general/DemoLogin.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { login } from "@/app/lib/auth";
+
+export default function DemoLogin() {
+  const router = useRouter();
+  const [email, setEmail] = useState(process.env.NEXT_PUBLIC_DEMO_LOGIN);
+  const [password, setPassword] = useState(
+    process.env.NEXT_PUBLIC_DEMO_LOGIN_PASSWORD
+  );
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setError(null);
+    setSuccess(null);
+
+    try {
+      await login(email, password);
+      setSuccess("User logged in successfully!");
+      setEmail("");
+      setPassword("");
+      router.push("/");
+    } catch (error) {
+      console.error("Error logging in with email and password: ", error);
+      setError(
+        "Failed to log in. Please check your credentials and try again."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+  return <div></div>;
+}
